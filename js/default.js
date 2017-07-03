820c69cd88e027459162497a8620d9ebf8f5b0a8
inaly17 <inaly0102@softbank.ne.jp>
2017-07-03 19:04:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -62,18 +62,18 @@ function getFileName(){
   return window.location.href.split('/').pop();
 }
 
-var filename=getFileName();
+var filename = getFileName();
 var opt;
-if(filename==='other.html'){
-  opt=document.querySelector('option[value="other.html"]');
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
 }else{
-  opt=document.querySelector('option[value="index.html"]');
+  opt = document.querySelector('option[value="index.html"]');
 }
 opt.selected=true;
 
 
-document.getElementById('form').select.onchange=function(){
-  location.href=document.getElementById('form').select.value;
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
 }
 
 var separate_time=function(time){
