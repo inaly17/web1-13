271ae2277d8e43039036de013d6327fe0154bf0e
inaly17 <inaly0102@softbank.ne.jp>
2017-07-03 18:22:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,4 @@
-var update=function(){
+
 
 
 var thmubs=document.querySelectorAll('.thumb');
@@ -86,15 +86,15 @@ var separate_time=function(time){
   
 
 
-
-  var now =new Date();
-  var target=new Date(2020,7,24,0,0,0,0);
-  var diff=target.getTime()-now.getTime();
-  var counter =separate_time(diff);
+var update = function(){
+  var now = new Date();
+  var target = new Date(2020,7,24,0,0,0,0);
+  var diff = target.getTime()-now.getTime();
+  var counter = separate_time(diff);
   document.getElementById('countdown').textContent=
     '東京オリンピックまであと　'+
       counter[3]+'日'+
-      counter[2]+'時'+
+      counter[2]+'時間'+
       counter[1]+'分'+
       counter[0]+'秒';
 refresh();
